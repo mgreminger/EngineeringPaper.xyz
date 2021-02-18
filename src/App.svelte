5021d85343c1f81bfff2dbead6d87f0df1e4ad8f
mgreminger <michael.greminger@gmail.com>
2021-02-18 17:43:58 -0600

    Add mathquill svelte component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,13 @@
 <script>
-	export let name;
+	import MathField from "./MathField.svelte"
+
+	let mathFieldLatex = "x=";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+
+<MathField bind:mathFieldLatex></MathField>
+<div>{mathFieldLatex}</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/MathField.svelte
@@ -0,0 +1,23 @@
+<script>
+  import {onMount} from "svelte";
+
+  export let mathFieldLatex = "";
+
+  let mathSpan;
+  let mathField;
+  let MQ = MathQuill.getInterface(2);
+
+  onMount(() => {
+    mathField = MQ.MathField(mathSpan, {
+      handlers: {
+        edit: () => {
+          mathFieldLatex = mathField.latex();
+        }
+      }
+    });
+
+  });
+
+</script>
+
+<span bind:this={mathSpan}>{mathFieldLatex}</span>
