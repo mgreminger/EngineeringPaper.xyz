ad3bf2ee869325ee4c4f892f2c6f01b8a89c9fcf
mgreminger <michael.greminger@gmail.com>
2021-04-21 20:55:34 -0500

    Use strict mode in worker

--- public/webworker.js
+++ public/webworker.js
@@ -1,11 +1,13 @@
-importScripts('pyodide/pyodide.js');
+"use strict";
+
+self.importScripts('pyodide/pyodide.js');
 
 let pyodide_ready = false;
 let interruptBuffer = new Int8Array(1);
 let py_funcs;
 let recursionError = false;
 
-pyodide_promise = self.loadPyodide({indexURL: 'pyodide/'})
+const pyodide_promise = self.loadPyodide({indexURL: 'pyodide/'})
 .then(() => self.pyodide.loadPackage('sympy'))
 .then(() => fetch("dimensional_analysis.py"))
 .then(response => response.text())
@@ -19,29 +21,29 @@ pyodide_promise = self.loadPyodide({indexURL: 'pyodide/'})
              console.error(e);}
 )
 
-onmessage = async function(e){
+self.onmessage = async function(e){
   await pyodide_promise;
 
   if (e.data.cmd === "solve") {
     if (!pyodide_ready) {
-      postMessage("pyodide_not_available");
+      self.postMessage("pyodide_not_available");
       return;
     }
     if (recursionError) {
-      postMessage("max_recursion_exceeded");
+      self.postMessage("max_recursion_exceeded");
       return;
     }
     try {
-      result = py_funcs.getQueryValues(e.data.data);
-      postMessage(JSON.parse(result));
+      const result = py_funcs.getQueryValues(e.data.data);
+      self.postMessage(JSON.parse(result));
     } catch (e) {
       // recursion is instance of InternalError in Firefox and RangeError in Chrome
       if (e instanceof RangeError || e instanceof InternalError) {
         recursionError = true;
-        postMessage("max_recursion_exceeded");
+        self.postMessage("max_recursion_exceeded");
         return;
       } else {
-        postMessage({error: "Unhandled exception occurred during Python call", results: []})
+        self.postMessage({error: "Unhandled exception occurred during Python call", results: []})
       }
     }
   }
