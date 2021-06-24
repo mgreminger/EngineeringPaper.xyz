758e43f74f79056e30e1cacc5f1c8a469f3ee69c
mgreminger <michael.greminger@gmail.com>
2021-06-24 12:38:10 -0500

    Create promise for loading pyodide and modules

--- public/webworker.js
+++ public/webworker.js
@@ -6,18 +6,22 @@ let pyodide_ready = false;
 let py_funcs;
 let recursionError = false;
 
-const pyodide_promise = self.loadPyodide({indexURL: 'pyodide/'})
-.then(() => self.pyodide.loadPackage('sympy'))
-.then(() => fetch("dimensional_analysis.py"))
-.then(response => response.text())
-.then((data) => {
-               py_funcs = self.pyodide.runPython(data);
-               console.log('Python Ready');
-               pyodide_ready = true
-             })
-.catch(e => {console.error('Python loading failed.');
-             console.error(e);}
-)
+const pyodide_promise = new Promise( async (resolve, reject) => { 
+  try {
+    await self.loadPyodide({indexURL: 'pyodide/'});
+    await self.pyodide.loadPackage('sympy');
+    const response = await fetch("dimensional_analysis.py");
+    const data = await response.text();
+    py_funcs = await self.pyodide.runPythonAsync(data);
+    console.log('Python Ready');
+    pyodide_ready = true
+    resolve();
+  } catch(e) {
+    console.error('Pyodide failed to load.');
+    console.log(e);
+    reject('Pyodide failed to load')
+  }
+});
 
 self.onmessage = async function(e){
   await pyodide_promise;
