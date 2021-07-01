23605292fe10a1f219ce2e870a3b658c89e566e8
mgreminger <michael.greminger@gmail.com>
2021-07-01 00:44:36 -0500

    Update error message when pyodide loading times out.

--- src/App.svelte
+++ src/App.svelte
@@ -83,7 +83,7 @@
 
     pyodideLoadingTimeoutRef = setTimeout(() => {
       if(!pyodideLoaded) {
-        error = "Pyodide failed to load. Refreshing page may help. Safari, iOS, and Android Chrome are not supported.";
+        error = "Pyodide failed to load. Refreshing page may help. Safari, iOS, and Android are not supported.";
       }
     }, pyodideLoadingTimeoutLength);
   }
