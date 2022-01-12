faaab0dab0960fb78de101d70217cb35cf8bd45d
mgreminger <michael.greminger@gmail.com>
2022-01-12 15:30:56 -0600

    Bump to final version of Pyodide 0.19.0

--- playwright/test_basic.js
+++ playwright/test_basic.js
@@ -7,7 +7,7 @@ const headless = false;
 // number of digits of accuracy after decimal point for .toBeCloseTo() calls
 const precision = 13; 
 
-[chromium, firefox].forEach(async (currentBrowser) => {
+[chromium, firefox, webkit].forEach(async (currentBrowser) => {
 
   let args = [];
   if (currentBrowser === chromium) {
