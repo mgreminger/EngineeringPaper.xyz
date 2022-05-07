0634c35ed2c890442a800f11cebb6de634afd88f
mgreminger <michael.greminger@gmail.com>
2022-05-07 10:59:01 -0500

    feat: add cloudflare worker to prefetch sheet data

---
+++ functions/[path].js
@@ -0,0 +1,28 @@
+const apiUrl = "https://engineeringpaper.herokuapp.com";
+
+export async function onRequest(context) {
+  const path = context.params.path;
+  const response = await fetch(context.request);
+  
+  if (!path.contains('.') && path !== "/") {
+    const contentType = response.headers.get('Content-Type');
+
+    // only attempt transform if response is html
+    if (contentType.startsWith('text/html')) {
+      return new HTMLRewriter().on('#prefetch', new AddSheet(path)).transform(response);
+    } else {
+      return response;
+    }
+  } else {
+    return response;
+  }
+}
+
+class AddSheet{
+  constructor(path) {
+    this.path = path;
+  }
+  element(element) {
+    element.setInnerContent(`prefetchedSheet = fetch(${apiUrl}${this.path})`);
+  }
+}
