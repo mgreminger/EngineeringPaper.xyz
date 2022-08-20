6db79b5f437ad3efd3e18a6b704b819d692275e0
mgreminger <michael.greminger@gmail.com>
2022-08-19 20:56:01 -0500

    tests: try self-hosted again
    Fixed issue on mac mini where port 5000 interfered with the tests

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -27,8 +27,7 @@ const config = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'list',
   reportSlowTests: null,
