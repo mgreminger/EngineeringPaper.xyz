eccf90431c9f6949c69c157fb781ca7dee85d2b7
mgreminger <michael.greminger@gmail.com>
2022-08-19 19:52:40 -0500

    tests: switch back to 1 worker for test runner

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -28,7 +28,7 @@ const config = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'list',
   reportSlowTests: null,
