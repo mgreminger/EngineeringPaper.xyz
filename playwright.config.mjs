e088fef2c36435f3dd949bb2151b0ffcf5cf855a
mgreminger <michael.greminger@gmail.com>
2022-08-19 19:00:44 -0500

    tests: switch back to github runner
    Also trying 2 processes instead of 1

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -27,7 +27,8 @@ const config = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  workers: 8,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'list',
   reportSlowTests: null,
