45ac5e8392909e0854784d53ff7e542766938c7f
mgreminger <michael.greminger@gmail.com>
2022-12-31 16:04:49 -0600

    Fix formatting of terms and conditions

--- src/Terms.svelte
+++ src/Terms.svelte
@@ -1,9 +1,13 @@
+<p><em>Updated December 31, 2022</em></p>
+<br>
 <p>Welcome to EngineeringPaper.xyz! By using this software, you are agreeing to the following terms
   and conditions.
 </p>
 
+<br>
+
 <h4>Introduction</h4>
-<em>Updated December 31, 2022</em>
+
 <p>
 EngineeringPaper.xyz strives to protect user privacy. This is a free service that doesn't require you to 
 create an account. All of the calculations you perform on EngineeringPaper.xyz are 
@@ -11,7 +15,7 @@ performed on your own computer and nothing you enter into your sheets is sent to
 When you save a sheet, we only store the minimum amount of information required to run the service (see details below).
 EngineeringPaper.xyz doesn't use cookies to track its users. 
 </p>
-
+<br>
 <h4>No Warranty</h4>
 <p>
 THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN 
@@ -21,7 +25,7 @@ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE R
 AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF 
 ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
 </p>
-
+<br>
 <h4>Privacy</h4>
 <p>
 In order to keep this service running smoothly, EngineeringPaper.xyz uses
@@ -36,7 +40,7 @@ to protect this free service from abuse. This database server is currently locat
 to other servers throughout the world once the data service is migrated to Cloudflare. Cloudflare automatically attempts to locate
 data closest to the user to improve overall performance.
 </p>
-
+<br>
 <h4>No Data Retention Guarantee</h4>
 <p>
   Our goal is to store sheets uploaded to EngineeringPaper.xyz for the life of the service. However, we provide
