6a96ff68d2e32020bc54f430b952e5d5d20d88be
mgreminger <michael.greminger@gmail.com>
2022-12-31 16:00:40 -0600

    Update terms and conditions for cloudflare analytics
    Also, keep track of terms version accepted

--- src/Terms.svelte
+++ src/Terms.svelte
@@ -1,15 +1,15 @@
-<p>Welcome to EngineeringPaper! By using this software, you are agreeing to the following terms
+<p>Welcome to EngineeringPaper.xyz! By using this software, you are agreeing to the following terms
   and conditions.
 </p>
 
 <h4>Introduction</h4>
-
+<em>Updated December 31, 2022</em>
 <p>
 EngineeringPaper.xyz strives to protect user privacy. This is a free service that doesn't require you to 
 create an account. All of the calculations you perform on EngineeringPaper.xyz are 
-performed on your own computer and nothing is sent to our servers unless you save a sheet. When you save a sheet, we 
-only store the minimum amount of information required to run the service (see details below). EngineeringPaper.xyz 
-doesn't use cookies and doesn't track its users. 
+performed on your own computer and nothing you enter into your sheets is sent to our servers unless you save a sheet.
+When you save a sheet, we only store the minimum amount of information required to run the service (see details below).
+EngineeringPaper.xyz doesn't use cookies to track its users. 
 </p>
 
 <h4>No Warranty</h4>
@@ -24,14 +24,17 @@ ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
 
 <h4>Privacy</h4>
 <p>
-EngineeringPaper strives to protect your privacy and only stores information relevant to any sheets that 
-you upload to the database. The only information that EngineeringPaper collects is your IP address when 
-you use the "Get Shareable Link" feature of this site. This IP address is stored in a database along with 
+In order to keep this service running smoothly, EngineeringPaper.xyz uses
+<a target="_blank" href="https://www.cloudflare.com/web-analytics/">Cloudflare's privacy protecting analytics service</a>
+to collect usage statistics. This analytics service does not use cookies to track users and does not store any personal information.
+</p>
+<p>
+The only time personal information is collected is when you use the "Get Shareable Link" feature of this site. When creating a shareable 
+link, your IP address is stored in a database along with 
 the contents of the sheet that is uploaded. The reason we collect the IP address for uploaded sheets is 
-to protect this free service from abuse. The database server is located in the United States. Other than 
-the IP address stored with uploaded sheets, this site does not track users and does not use cookies. 
-If you don't use the "Get Shareable Link" feature, no information is stored on our servers, even if you 
-view links shared by others.
+to protect this free service from abuse. This database server is currently located in the United States. This data may be relocated
+to other servers throughout the world once the data service is migrated to Cloudflare. Cloudflare automatically attempts to locate
+data closest to the user to improve overall performance.
 </p>
 
 <h4>No Data Retention Guarantee</h4>
@@ -39,5 +42,5 @@ view links shared by others.
   Our goal is to store sheets uploaded to EngineeringPaper.xyz for the life of the service. However, we provide
   no guarantee to the accessibility of this data in the future. We reserve the right to shut down this 
   service at any time and for any reason without warning. We also reserve the right to delete any uploaded 
-  sheet at any time and for any reason without warning.
+  sheet at any time, and for any reason, without warning.
 </p>
