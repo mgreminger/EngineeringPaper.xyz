dd6d70eba0b61289d668cbb1e6d50842fd653976
mgreminger <michael.greminger@gmail.com>
2023-01-05 01:50:35 -0600

    tests: update dev mode app url to prevent CORS errors

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -36,7 +36,7 @@ const config = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.APP_URL ? process.env.APP_URL : 'http://localhost:8788',
+    baseURL: process.env.APP_URL ? process.env.APP_URL : 'http://127.0.0.1:8788',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
