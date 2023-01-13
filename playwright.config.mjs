95a9773c56e3f728958e4536cfbf52eeef34fe04
mgreminger <michael.greminger@gmail.com>
2023-01-12 20:53:03 -0600

    tests: increase default timeout to 10 seconds

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -21,7 +21,7 @@ const config = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 10000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
