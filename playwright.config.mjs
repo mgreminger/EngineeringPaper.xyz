b52b818d86980dad94f4199c8b59b1498cae869c
mgreminger <michael.greminger@gmail.com>
2023-03-31 14:26:34 -0500

    tests: adjust actionTimeout

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -34,7 +34,7 @@ const config = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 10000,
+    actionTimeout: 100000,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.APP_URL ? process.env.APP_URL : 'http://127.0.0.1:8788',
 
