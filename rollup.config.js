9ee735651dec42b85c01393c94a46c5fb517d5af
mgreminger <michael.greminger@gmail.com>
2023-04-26 23:23:33 -0500

    fix: no longer need to copy mathlive.min.js during build

--- rollup.config.js
+++ rollup.config.js
@@ -68,7 +68,6 @@ export default [
 			targets: [
 				{src: 'node_modules/mathlive/dist/fonts/*', dest: 'public/build/mathlive/fonts'},
 				{src: 'node_modules/mathlive/dist/sounds/*', dest: 'public/build/mathlive/sounds'},
-				{src: 'node_modules/mathlive/dist/mathlive.min.js', dest: 'public/build/mathlive/'},
 			]
 		}),
 
