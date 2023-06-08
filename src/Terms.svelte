0f13e1042146626273d75cc515dfedeada7602da
mgreminger <michael.greminger@gmail.com>
2023-06-08 16:52:12 -0500

    Update terms and conditions Add wording on limitation of liability

--- src/Terms.svelte
+++ src/Terms.svelte
@@ -3,63 +3,70 @@
 </script>
 
 <p><em>Updated {versionDateString}</em></p>
-<br>
-<p>Welcome to EngineeringPaper.xyz! By using this software, you are agreeing to the following terms
-  and conditions.
+<br />
+<p>
+  Welcome to EngineeringPaper.xyz! By using this software, you are agreeing to
+  the following terms and conditions.
 </p>
 
-<br>
+<br />
 
 <h4>Introduction</h4>
 
 <p>
-EngineeringPaper.xyz strives to protect user privacy. This is free and open source software that does not 
-require you to create an account to use. All of the calculations you perform on EngineeringPaper.xyz are 
-performed on your own computer and nothing you enter into your sheets is sent to our servers unless you create
-a shareable link.
-When you create a shareable link, we only store the minimum amount of information required to run the 
-shareable link service (see details below).
+  EngineeringPaper.xyz strives to protect user privacy. This is free and open
+  source software that does not require you to create an account to use. All of
+  the calculations you perform on EngineeringPaper.xyz are performed on your own
+  computer and nothing you enter into your sheets is sent to our servers unless
+  you create a shareable link. When you create a shareable link, we only store
+  the minimum amount of information required to run the shareable link service
+  (see details below).
 </p>
-<br>
+<br />
 <p>
-You have the option to save your sheets to your local drive. If you only save your sheets locally,
-none of your data is ever sent to our servers.
+  You have the option to save your sheets to your local drive. If you only save
+  your sheets locally, none of your data is ever sent to our servers.
 </p>
-<br>
-<h4>No Warranty</h4>
+<br />
+<h4>No Warranty and Limitation of Liability</h4>
 <p>
-THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN 
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM “AS IS” 
-WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY 
-AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF 
-ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+  AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+  LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+  PERFORMANCE OF THIS SOFTWARE.
 </p>
-<br>
+<br />
 <h4>Privacy</h4>
 <p>
-  EngineeringPaper.xyz does not use cookies and does not track its users. 
-The only time personal information is collected is when you use the "Get Shareable Link" feature of this site. 
-When creating a shareable link, your IP address is stored in a database along with 
-the contents of the sheet that is uploaded. The reason we collect the IP address for uploaded sheets is 
-to protect this free shareable link service from abuse. When creating a shareable link, your data is stored on Cloudflare's 
-global network. Cloudflare automatically attempts to locate
-data closest to the user to improve overall performance.
+  EngineeringPaper.xyz does not use cookies and does not track its users. The
+  only time personal information is collected is when you use the "Get Shareable
+  Link" feature of this site. When creating a shareable link, your IP address is
+  stored in a database along with the contents of the sheet that is uploaded.
+  The reason we collect the IP address for uploaded sheets is to protect this
+  free shareable link service from abuse. When creating a shareable link, your
+  data is stored on Cloudflare's global network. Cloudflare automatically
+  attempts to locate data closest to the user to improve overall performance.
 </p>
-<br>
+<br />
 <p>
   Saving your sheets locally avoids sending any of your data to our database.
 </p>
-<br>
+<br />
 <h4>No Data Retention Guarantee for Shareable Links</h4>
 <p>
-  Our goal is to store sheets using the shareable link feature for the life of the shareable link service.
-  However, we provide no guarantee to the accessibility of shareable links in the future. We reserve the right
-  to shut down the shareable link service at any time, and for any reason, without warning.
-  We also reserve the right to delete any uploaded sheet at any time, and for any reason, without warning.
+  Our goal is to store sheets using the shareable link feature for the life of
+  the shareable link service. However, we provide no guarantee to the
+  accessibility of shareable links in the future. We reserve the right to shut
+  down the shareable link service at any time, and for any reason, without
+  warning. We also reserve the right to delete any uploaded sheet at any time,
+  and for any reason, without warning.
 </p>
-<br>
+<br />
 <p>
- To ensure future access to your sheets, be sure to save them locally (even if you have created a shareable link).
+  To ensure future access to your sheets, be sure to save them locally (even if
+  you have created a shareable link).
 </p>
-<br>
+<br />
