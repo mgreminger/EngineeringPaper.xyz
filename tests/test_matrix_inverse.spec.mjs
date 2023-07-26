7f3f52719b5516036f02f65ac8fbe4a4e375bc06
mgreminger <michael.greminger@gmail.com>
2023-07-25 20:09:00 -0500

    tests: complete matrix inverse tests
    Tests not passing yet

--- tests/test_matrix_inverse.spec.mjs
+++ tests/test_matrix_inverse.spec.mjs
@@ -19,7 +19,6 @@ test('Matrix inverse numeric', async () => {
 
     let content = await page.textContent(`#result-value-0`);
     expect(content).toBe(String.raw`\begin{bmatrix} 2 & -3 \\ -1 & 2 \end{bmatrix}`);
-
   });
 
   test('Matrix inverse numeric with units', async () => {
@@ -29,7 +28,6 @@ test('Matrix inverse numeric', async () => {
 
     let content = await page.textContent(`#result-value-0`);
     expect(content).toBe(String.raw`\begin{bmatrix} 2\left\lbrack \frac{1}{m}\right\rbrack  & -3\left\lbrack \frac{1}{m}\right\rbrack  \\ -1\left\lbrack \frac{1}{m}\right\rbrack  & 2\left\lbrack \frac{1}{m}\right\rbrack  \end{bmatrix}`);
-
   });
 
   test('Matrix exponent inverse numeric with units', async () => {
@@ -39,7 +37,6 @@ test('Matrix inverse numeric', async () => {
 
     let content = await page.textContent(`#result-value-0`);
     expect(content).toBe(String.raw`\begin{bmatrix} 2\left\lbrack \frac{1}{m}\right\rbrack  & -3\left\lbrack \frac{1}{m}\right\rbrack  \\ -1\left\lbrack \frac{1}{m}\right\rbrack  & 2\left\lbrack \frac{1}{m}\right\rbrack  \end{bmatrix}`);
-
   });
 
   test('Matrix inverse with variable matrix', async () => {
@@ -51,7 +48,6 @@ test('Matrix inverse numeric', async () => {
 
     let content = await page.textContent(`#result-value-1`);
     expect(content).toBe(String.raw`\begin{bmatrix} 2 & -3 \\ -1 & 2 \end{bmatrix}`);
-
   });
 
   test('Matrix exponent inverse with variable matrix', async () => {
@@ -63,9 +59,7 @@ test('Matrix inverse numeric', async () => {
 
     let content = await page.textContent(`#result-value-1`);
     expect(content).toBe(String.raw`\begin{bmatrix} 2 & -3 \\ -1 & 2 \end{bmatrix}`);
-
   });
-
 
   test('Matrix inverse with mixed units', async () => {
     await page.setLatex(0, String.raw`\mathrm{inv}\left(\begin{bmatrix}2\left\lbrack m\right\rbrack & 3\left\lbrack s\right\rbrack\\ 1\left\lbrack m\right\rbrack & 2\left\lbrack s\right\rbrack\end{bmatrix}\right)=`);
@@ -74,5 +68,69 @@ test('Matrix inverse numeric', async () => {
 
     let content = await page.textContent(`#result-value-0`);
     expect(content).toBe(String.raw`\begin{bmatrix} 2\left\lbrack \frac{1}{m}\right\rbrack  & -3\left\lbrack \frac{1}{m}\right\rbrack  \\ -1\left\lbrack Hz\right\rbrack  & 2\left\lbrack Hz\right\rbrack  \end{bmatrix}`);
-
   });
+  
+  test('Matrix inverse exponent with mixed units', async () => {
+    await page.setLatex(0, String.raw`\begin{bmatrix}2\left\lbrack m\right\rbrack & 3\left\lbrack s\right\rbrack\\ 1\left\lbrack m\right\rbrack & 2\left\lbrack s\right\rbrack\end{bmatrix}^{-1}=`);
+
+    await page.waitForSelector('text=Updating...', {state: 'detached'});
+
+    let content = await page.textContent(`#result-value-0`);
+    expect(content).toBe(String.raw`\begin{bmatrix} 2\left\lbrack \frac{1}{m}\right\rbrack  & -3\left\lbrack \frac{1}{m}\right\rbrack  \\ -1\left\lbrack Hz\right\rbrack  & 2\left\lbrack Hz\right\rbrack  \end{bmatrix}`);
+  });
+
+  test('Matrix inverse with inconsistent units', async () => {
+    await page.setLatex(0, String.raw`\mathrm{inv}\left(\begin{bmatrix}2\left\lbrack m\right\rbrack & 3\left\lbrack s\right\rbrack\\ 1\left\lbrack s\right\rbrack & 2\left\lbrack s\right\rbrack\end{bmatrix}\right)=`);
+
+    await page.waitForSelector('text=Updating...', {state: 'detached'});
+
+    await expect(page.locator('#cell-0 >> text=Dimension Error')).toBeAttached();
+  });
+
+  test('Matrix inverse exponent with inconsistent units', async () => {
+    await page.setLatex(0, String.raw`\begin{bmatrix}2\left\lbrack m\right\rbrack & 3\left\lbrack s\right\rbrack\\ 1\left\lbrack s\right\rbrack & 2\left\lbrack s\right\rbrack\end{bmatrix}^{-1}=`);
+
+    await page.waitForSelector('text=Updating...', {state: 'detached'});
+
+    await expect(page.locator('#cell-0 >> text=Dimension Error')).toBeAttached();
+  });
+
+  test('Matrix inverse keyboard entry', async () => {
+    await page.locator('#cell-0 >> math-field.editable').type('1[m]*inv([2,2');
+    await page.locator('#cell-0 >> math-field.editable').press('Enter');
+    await page.locator('#cell-0 >> math-field.editable').type('2');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').type('3');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').type('1');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').type('2');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+
+    await page.locator('#cell-0 >> math-field.editable').type('@');
+
+    await page.locator('#cell-0 >> math-field.editable').type('[2,2');
+    await page.locator('#cell-0 >> math-field.editable').press('Enter');
+    await page.locator('#cell-0 >> math-field.editable').type('2[m]');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').type('-3[m]');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').type('-1[m]');
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').type('2[m]');
+
+    await page.locator('#cell-0 >> math-field.editable').press('ArrowRight');
+    await page.locator('#cell-0 >> math-field.editable').type('^-1');
+
+    await page.locator('#cell-0 >> math-field.editable').press('Tab');
+    await page.locator('#cell-0 >> math-field.editable').type('=');
+
+    await page.waitForSelector('text=Updating...', {state: 'detached'});
+
+    let content = await page.textContent(`#result-value-0`);
+    expect(content).toBe(String.raw`\begin{bmatrix} 1 & 0 \\ 0 & 1 \end{bmatrix}`);
+  });
+
+
+
