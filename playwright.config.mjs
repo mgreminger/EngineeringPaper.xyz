9f2bcf58fa679a0b9ab828bfc7aa18ffb9655bc4
mgreminger <michael.greminger@gmail.com>
2024-01-21 14:45:33 -0600

    test: try to add webkit in CI
    The new GitHub runners may be able to handle adding webkit tests. The old runners were not able to reliable handle the webkit runs.

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -43,8 +43,7 @@ const config = {
   },
 
   /* Set projects for CI and local environments */
-  projects: process.env.CI ? projectFactory(['chromium', 'firefox']) : 
-                             projectFactory(['chromium', 'firefox', 'webkit']),
+  projects: projectFactory(['chromium', 'firefox', 'webkit']),
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   outputDir: 'test-results/',
