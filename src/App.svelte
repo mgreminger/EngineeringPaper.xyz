c8dd4eb716136e2c94716c346b5defabe4749777
mgreminger <michael.greminger@gmail.com>
2024-03-17 17:23:18 -0500

    Bump version and add update description

--- src/App.svelte
+++ src/App.svelte
@@ -90,7 +90,7 @@
 
   const apiUrl = window.location.origin;
 
-  const currentVersion = 20240227;
+  const currentVersion = 20240318;
   const tutorialHash = "fFjTsnFoSQMLwcvteVoNtL";
 
   const termsVersion = 20240110;
