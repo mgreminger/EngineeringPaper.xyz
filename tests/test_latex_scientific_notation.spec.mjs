b3c4bb2288684224c16e9aa05563deffc70a7b59
mgreminger <michael.greminger@gmail.com>
2024-08-24 10:53:38 -0500

    tests: add some order of operations tests for latex scientific notation

--- tests/test_latex_scientific_notation.spec.mjs
+++ tests/test_latex_scientific_notation.spec.mjs
@@ -64,3 +64,38 @@ test('Test exponent error checking for exponents applied to numbers with units '
   content = await page.textContent('#result-units-0');
   expect(content).toBe('m^2');
 });
+
+test('Test latex scientific notation order of operations', async () => {
+  await page.setLatex(0, String.raw`\left(-2\cdot10^4\left\lbrack mm\right\rbrack+2\left\lbrack m\right\rbrack\right)\cdot2=`);
+
+  await page.locator('#add-math-cell').click();
+  await page.setLatex(1, String.raw`\left(1\left\lbrack m\right\rbrack-2\cdot10^4\left\lbrack mm\right\rbrack\right)\cdot2=`);
+
+  await page.locator('#add-math-cell').click();
+  await page.setLatex(2, String.raw`\left(-\left(4\cdot10^4\left\lbrack m^2\right\rbrack\right)^{\frac12}+2000\left\lbrack mm\right\rbrack\right)\cdot2=`);
+
+  await page.locator('#add-math-cell').click();
+  await page.setLatex(3, String.raw`\left(2\cdot10^4\left\lbrack m^2\right\rbrack+20000\left\lbrack m^2\right\rbrack\right)^{\frac12}\cdot2=`);
+
+  await page.waitForSelector('text=Updating...', {state: 'detached'});
+
+  let content = await page.textContent('#result-value-0');
+  expect(parseLatexFloat(content)).toBeCloseTo(-36, precision);
+  content = await page.textContent('#result-units-0');
+  expect(content).toBe('m');
+
+  content = await page.textContent('#result-value-1');
+  expect(parseLatexFloat(content)).toBeCloseTo(-38, precision);
+  content = await page.textContent('#result-units-1');
+  expect(content).toBe('m');
+
+  content = await page.textContent('#result-value-2');
+  expect(parseLatexFloat(content)).toBeCloseTo(-396, precision);
+  content = await page.textContent('#result-units-2');
+  expect(content).toBe('m');
+
+  content = await page.textContent('#result-value-3');
+  expect(parseLatexFloat(content)).toBeCloseTo(400, precision);
+  content = await page.textContent('#result-units-3');
+  expect(content).toBe('m');
+});
