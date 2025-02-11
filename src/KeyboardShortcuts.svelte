ec5d4805d09bf545487fb87b245bdd7fd90ff12f
mgreminger <michael.greminger@gmail.com>
2025-02-11 09:38:17 -0600

    feat: update keyboard shortcuts modal
    Add formula and other quill shortcuts and categorize shortcuts. Also, add calc shortcuts that were previously undocumented.

--- src/KeyboardShortcuts.svelte
+++ src/KeyboardShortcuts.svelte
@@ -9,6 +9,7 @@
     border-collapse: collapse;
     margin: 1rem 0;
   }
+
   table,
   th,
   td {
@@ -25,8 +26,10 @@
     text-align: left;
     vertical-align: middle;
   }
+
   th {
     font-weight: bold;
+    background-color: lightgray;
   }
 
   span.key {
@@ -39,11 +42,106 @@
 </style>
 
 <table>
+  <thead>
+    <tr>
+      <th colspan="2">Global Shortcuts</th>
+    </tr>
+  </thead>
   <tbody>
     <tr>
-      <th class="first-column">Key Combination</th>
-      <th>Action</th>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">S</span></td>
+      <td>Save current sheet to local file</td>
     </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Shift</span> + <span class="key">S</span></td>
+      <td>Save current sheet to local file with a new filename instead of replacing existing file </td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">L</span></td>
+      <td>Save current sheet to database to get a shareable link</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">O</span></td>
+      <td>Open sheet from local file (replaces currently loaded sheet)</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Shift</span> + <span class="key">N</span></td>
+      <td>Load blank sheet (replaces currently loaded sheet)</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">&uarr;</span></td>
+      <td>Select cell above current cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">&darr;</span></td>
+      <td>Select cell below current cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">D</span></td>
+      <td>Delete current cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">Enter</span></td>
+      <td
+        >Insert Math Cell when in a Math Cell, or add row when in a Plot, Table,
+        Piecewise Expression, or System Solve Cell</td
+      >
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">Shift</span> + <span class="key">Enter</span></td>
+      <td>Insert Math Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">1</span></td>
+      <td>Insert Math Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">2</span></td>
+      <td>Insert Documentation Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">3</span></td>
+      <td>Insert Plot Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">4</span></td>
+      <td>Insert Selector Table Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">5</span></td>
+      <td>Insert Data Table Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">6</span></td>
+      <td>Insert Piecewise Expression Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">7</span></td>
+      <td>Insert System Solve Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">8</span></td>
+      <td>Insert Fluid Properties Cell</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">9</span></td>
+      <td>Insert Sheet</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">Esc</span></td>
+      <td>Unselect all cells</td>
+    </tr>
+  </tbody>
+</table>
+
+
+<table>
+  <thead>
+    <tr>
+      <th colspan="2">Math Field Shortcuts</th>
+    </tr>
+  </thead>
+  <tbody>
     <tr>
       <td class="first-column"><span class="key">*</span></td>
       <td>Multiply</td>
@@ -115,28 +213,68 @@
       <td>Toggle comment mode in a math field</td>
     </tr>
     <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">S</span></td>
-      <td>Save current sheet to local file</td>
+      <td class="first-column"><span>Type $int</span></td>
+      <td>Inserts an integral</td>
     </tr>
     <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Shift</span> + <span class="key">S</span></td>
-      <td>Save current sheet to local file with a new filename instead of replacing existing file </td>
+      <td class="first-column"><span>Type $prime</span></td>
+      <td>Inserts a derivative</td>
     </tr>
     <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">L</span></td>
-      <td>Save current sheet to database to get a shareable link</td>
+      <td class="first-column"><span>Type $doubleprime</span></td>
+      <td>Inserts a double derivative</td>
     </tr>
     <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">O</span></td>
-      <td>Open sheet from local file (replaces currently loaded sheet)</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Shift</span> + <span class="key">N</span></td>
-      <td>Load blank sheet (replaces currently loaded sheet)</td>
+      <td class="first-column"><span>Type $tripleprime</span></td>
+      <td>Inserts a triple derivative</td>
     </tr>
     <tr>
       <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Z</span></td>
-      <td>Undo edit in current math or text field</td>
+      <td>Undo edit in current math field</td>
+    </tr>
+
+    <tr>
+      {#if modifier === "Ctrl"}
+        <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Y</span></td>
+      {:else}
+        <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Shift</span> + <span class="key">Z</span></td>
+      {/if}
+      <td>Redo edit in current math field</td>
+    </tr>
+  </tbody>
+</table>
+
+
+<table>
+  <thead>
+    <tr>
+      <th colspan="2">Documentation Field Shortcuts</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">B</span></td>
+      <td>Set selected text to Bold</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">I</span></td>
+      <td>Set selected text to Italics</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">U</span></td>
+      <td>Set selected text to Underline</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">K</span></td>
+      <td>Insert link at selected text</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">E</span></td>
+      <td>Insert formula at cursor location (uses LaTeX notation)</td>
+    </tr>
+    <tr>
+      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Z</span></td>
+      <td>Undo edit in current documentation field</td>
     </tr>
     <tr>
       {#if modifier === "Ctrl"}
@@ -144,72 +282,7 @@
       {:else}
         <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Shift</span> + <span class="key">Z</span></td>
       {/if}
-      <td>Redo edit in current math or text field</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">&uarr;</span></td>
-      <td>Select cell above current cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">&darr;</span></td>
-      <td>Select cell below current cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">D</span></td>
-      <td>Delete current cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">Enter</span></td>
-      <td
-        >Insert Math Cell when in a Math Cell, or add row when in a Plot, Table,
-        Piecewise Expression, or System Solve Cell</td
-      >
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">Shift</span> + <span class="key">Enter</span></td>
-      <td>Insert Math Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">1</span></td>
-      <td>Insert Math Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">2</span></td>
-      <td>Insert Documentation Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">3</span></td>
-      <td>Insert Plot Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">4</span></td>
-      <td>Insert Selector Table Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">5</span></td>
-      <td>Insert Data Table Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">6</span></td>
-      <td>Insert Piecewise Expression Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">7</span></td>
-      <td>Insert System Solve Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">8</span></td>
-      <td>Insert Fluid Properties Cell</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">{modifier}</span> + <span class="key">Enter</span> then <span class="key">9</span></td>
-      <td>Insert Sheet</td>
-    </tr>
-    <tr>
-      <td class="first-column"><span class="key">Esc</span></td>
-      <td>Unselect all cells</td>
+      <td>Redo edit in current documentation field</td>
     </tr>
   </tbody>
 </table>
-
-
