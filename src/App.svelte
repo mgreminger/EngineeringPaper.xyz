1c21858c6f9568753c47efd334320f952bde4e62
mgreminger <michael.greminger@gmail.com>
2025-03-19 13:55:49 -0500

    tests: more filechooser cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -213,6 +213,7 @@
   // This is necessary since dismissing the unsaved changes dialog in playwright doesn't work after the first
   // time it is requested.
   (window as any).forceLoadBlankSheet = async () => {appState.unsavedChange = false; await loadBlankSheet();};
+  (window as any).forceNoUnsavedChange = () => {appState.unsavedChange = false;};
 
   // Used for testing to simplify the deleting of cells
   // The two-step delete, delete and then delete the undo delete cell, 
