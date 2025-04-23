2563fc0918d49ada41691600ca95e309401b8684
mgreminger <michael.greminger@gmail.com>
2025-04-22 22:52:22 -0500

    style: update variable accent virtual keyboard buttons

--- tests/test_calc.spec.mjs
+++ tests/test_calc.spec.mjs
@@ -19,7 +19,7 @@ test('test basic calculus', async () => {
   await page.setLatex(0, String.raw`\int _{0}^{pi}\left(sin\left(t\right)\right)d\left(t\right)=`);  
   
   await page.click('#add-math-cell');
-  await page.locator('button').filter({ hasText: 'n^∫Σ' }).click();
+  await page.locator('button').filter({ hasText: 'a^∫Σ' }).click();
   await page.click(':nth-match(button.keyboard:has-text("∫"), 2)');
   await page.type(':nth-match(math-field.editable, 2)', 'b');
   await page.press(':nth-match(math-field.editable, 2)', 'Tab');
@@ -41,7 +41,7 @@ test('test basic calculus', async () => {
   await page.click('#add-math-cell');
   await page.setLatex(5, String.raw`func=x^{3}\cdot y^{2}`);
   await page.click('#add-math-cell');
-  await page.locator('button').filter({ hasText: 'n^∫Σ' }).click();
+  await page.locator('button').filter({ hasText: 'a^∫Σ' }).click();
   await page.locator('button').filter({ hasText: 'd2dx2dx2d2' }).click();
   await page.type(':nth-match(math-field.editable, 7)', 'x');
   await page.press(':nth-match(math-field.editable, 7)', 'Tab');
