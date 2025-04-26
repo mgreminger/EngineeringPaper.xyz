4f5cc3ce324d8a2f54cc123b5cff6c12b8d9ebe5
mgreminger <michael.greminger@gmail.com>
2025-04-26 15:36:59 -0500

    fix: setup custom units in parser worker

--- src/parser/parserWorker.ts
+++ src/parser/parserWorker.ts
@@ -1,4 +1,7 @@
 import { parseLatex } from "./LatexToSympy";
+import { createCustomUnits } from "../utility";
+
+createCustomUnits();
 
 self.onmessage = function(e) {
     self.postMessage(parseLatex(e.data.latex, e.data.id, 0, e.data.type, e.data.dataTableInfo));
