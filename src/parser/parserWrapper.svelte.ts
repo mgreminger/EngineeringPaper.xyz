e546ffc26b0d0bb57760086de5a5e4a205456b45
mgreminger <michael.greminger@gmail.com>
2025-05-02 09:07:07 -0500

    refactor: update parser wrapper to dedup parser queue
    Only preform most recent parse when multiple parses are pending on the same math field

--- src/parser/parserWrapper.svelte.ts
+++ src/parser/parserWrapper.svelte.ts
@@ -2,28 +2,56 @@ import type { FieldTypes, DataTableInfo } from "./types";
 import type { ParsingResult } from "./LatexToSympy";
 import appState from "../stores.svelte";
 
+type ParseRequest = {
+  latex: string,
+  id: number,
+  type: FieldTypes,
+  dataTableInfo?: DataTableInfo
+};
+
 export class LatexParserWrapper {
   worker: Worker;
-  resolveFunctionQueue: ((input: ParsingResult) => void)[] = [];
+  requestQueue: Map<number, {request: ParseRequest, resolvers: ((input: ParsingResult) => void)[]}> = new Map();
+  currentResolvers: ((input: ParsingResult) => void)[] = [];
 
   constructor() {
     this.worker = new Worker('parserWorker.js');
 
     this.worker.onmessage = (e) => {
-      this.resolveFunctionQueue.shift()(e.data as ParsingResult);
-      if (this.resolveFunctionQueue.length === 0) {
+      for (const resolver of this.currentResolvers) {
+        resolver(e.data as ParsingResult);
+      }
+      this.currentResolvers = [];
+      if (this.requestQueue.size === 0) {
         appState.parsePending = false;
+      } else {
+        const [key, value] = this.requestQueue.entries().next().value;
+        this.currentResolvers = value.resolvers;
+        this.requestQueue.delete(key);
+        this.worker.postMessage(value.request);
       }
     };
   }
 
-  async parseLatex(latex: string, id: number, type: FieldTypes, dataTableInfo?: DataTableInfo): Promise<ParsingResult> {
-    this.worker.postMessage({latex, id, type, dataTableInfo});
+  parseLatex(latex: string, id: number, type: FieldTypes, dataTableInfo?: DataTableInfo): Promise<ParsingResult> {
 
-    appState.parsePending = true;
+    const newRequest: ParseRequest = {latex, id, type, dataTableInfo};
 
     return new Promise((resolve: (input: ParsingResult) => void, reject) => {
-      this.resolveFunctionQueue.push(resolve);
+      if (!appState.parsePending) {
+        this.worker.postMessage(newRequest);
+        this.currentResolvers = [resolve, ];
+      } else {
+        if (this.requestQueue.has(id)) {
+          const {request, resolvers} = this.requestQueue.get(id);
+          resolvers.push(resolve);
+          this.requestQueue.set(id, {request: newRequest, resolvers});
+        } else {
+          this.requestQueue.set(id, {request: newRequest, resolvers: [resolve,]})
+        }
+      }
+
+      appState.parsePending = true;
     });
   }
 }
