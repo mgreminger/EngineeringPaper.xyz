4b7528debc34c1ccd86f1fbe3c34b020c8eaa0e5
mgreminger <michael.greminger@gmail.com>
2026-02-05 09:14:10 -0600

    chore: update version and releases dialog

--- src/stores.svelte.ts
+++ src/stores.svelte.ts
@@ -64,7 +64,7 @@ type AppState = {
 }
 
 const appState: AppState = $state<AppState>({
-  currentVersion: 20251229,
+  currentVersion: 20260205,
   termsVersion: 20240110,
 
   unsavedChange: false,
