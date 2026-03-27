0c6d47c1be12bd61505d27dc935de935c2e03e82
mgreminger <michael.greminger@gmail.com>
2026-03-27 12:47:42 -0500

    tests: increase navigation timeout and update _routes.json

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -38,6 +38,8 @@ const config = {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.APP_URL ? process.env.APP_URL : 'http://127.0.0.1:8788',
 
+    navigationTimeout: 120 * 1000,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
