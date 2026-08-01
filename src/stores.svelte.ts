44f7e0dc735dfe0d9428ee45c8956f5d9f7319cc
Michael Greminger <michael.greminger@gmail.com>
2026-08-01 16:01:08 -0500

    chore: bump version and update releases modal

--- src/stores.svelte.ts
+++ src/stores.svelte.ts
@@ -64,7 +64,7 @@ type AppState = {
 }
 
 const appState: AppState = $state<AppState>({
-  currentVersion: 20260730,
+  currentVersion: 20260801,
   termsVersion: 20240110,
 
   unsavedChange: false,
