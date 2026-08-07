9efc2e543450a9b70a3cbd08f76494b27e695e43
Michael Greminger <michael.greminger@gmail.com>
2026-08-06 23:25:25 -0500

    fix: link video in .pro modal
    Cannot embed because of the CSP

--- src/ProInfo.svelte
+++ src/ProInfo.svelte
@@ -18,15 +18,6 @@
     border: 1px solid #e0e0e0;
     border-radius: 8px; 
   }
-
-  iframe {
-    width: 100%;
-    max-width: 800px; 
-    aspect-ratio: 16 / 9;
-    height: auto;
-    border-radius: 4px;
-    margin: 0 auto;
-  }
 </style>
 
 
@@ -34,10 +25,10 @@
   <p>
     You asked for it, and now it's here! 
     Introducing 
-    <strong><a href="https://engineeringpaper.pro" target="_blank" rel="noopener noreferrer">EngineeringPaper.pro</a></strong>, the full power of EngineeringPaper.xyz as a fully 
-    integrated desktop application. It has native file associations, a tabbed interface, 
-    local document generation (without a watermark), and is fully offline. 
-    Available with a perpetual license for Windows, Mac, and Linux. Try it free for 30 days at 
+    <strong><a href="https://engineeringpaper.pro" target="_blank" rel="noopener noreferrer">EngineeringPaper.pro</a></strong>, 
+    the full power of EngineeringPaper.xyz in a dedicated desktop application. It has native file associations, a tabbed interface, 
+    local document generation (without the watermark), and is fully offline. 
+    Available with a perpetual license for Windows, Mac, and Linux. Test it out free for 30 days at 
     <a href="https://engineeringpaper.pro" target="_blank" rel="noopener noreferrer">EngineeringPaper.pro</a>! 
   </p>
 
@@ -46,24 +37,16 @@
     full featured, free, and open source!
   </p>
 
+  <p>
+    See EngineeringPaper.pro in action with the new 
+    <a href="https://youtu.be/Mann5qJ9TGw?si=28ZZOC7ZrA5CsPw5" target="_blank">full tutorial</a>. 
+    Most of the tutorial applies to EngineeringPaper.xyz as well.
+  </p>
+
   <img
     src="images/pro_screenshot.webp"
     alt="EngineeringPaper.pro Screenshot"
     width="1600"
     height="1130"
   />
-
-  <p>
-    See EngineeringPaper.pro in action with the following full tutorial. Most of the 
-    tutorial applies to EngineeringPaper.xyz as well!
-  </p>
-
-  <iframe 
-    src="https://www.youtube-nocookie.com/embed/Mann5qJ9TGw?si=7L2pplq0cuJVUi63" 
-    title="EngineeringPaper.pro Tutorial" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-    referrerpolicy="strict-origin-when-cross-origin" 
-    allowfullscreen
-  ></iframe>
 </div>
